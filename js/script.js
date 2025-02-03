90d0df4d6b0cebae06a7bd1aa55cdc9e2993cc11
Wioletta Koperska <koperskawioletta@gmail.com>
2025-02-03 17:23:16 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,20 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+            done: false,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -17,20 +31,23 @@
             htmlString += `
             <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
                 ${task.content}
+                <button class="js-remove">Usuń</button>
             </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        const removeButtons = document.querySelectorAll(".js-remove");
+        
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
-
-        render();
     };
+
+    
 
     const onFormSubmit = (event) => {
         event.preventDefault();
